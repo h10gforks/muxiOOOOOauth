6bfbce41326c670f293846b211afb6a5ec28b455
neo1218 <neo1218@yeah.net>
2016-05-08 20:47:33 +0800

    set email unique

--- muxiOOOOOauth/app/models.py
+++ muxiOOOOOauth/app/models.py
@@ -79,7 +79,7 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(164), unique=True, index=True)
-    email = db.Column(db.String(164), info={'validator' : Email()})
+    email = db.Column(db.String(164), unique=True, info={'validator' : Email()})
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(164))
     sid = db.Column(db.String(20))
