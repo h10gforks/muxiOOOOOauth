31523048211d3346464d4d5525c088c2043cf220
neo1218 <neo1218@yeah.net>
2016-05-25 16:52:42 +0800

    update authentication field

--- muxiOOOOOauth/app/api/authentication.py
+++ muxiOOOOOauth/app/api/authentication.py
@@ -27,7 +27,7 @@ def verify_password(email_or_token, password):
         g.token_used = True
         return g.current_user is not None
 
-    user = User.query.filter_by(username=email_or_token).first()
+    user = User.query.filter_by(email=email_or_token).first()
     if not user:
         return False
 
